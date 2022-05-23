0e3d8000e81da0be2ee48ff9741191436a9888e0
caihua2300 <caihua2300@gmail.com>
2022-05-23 16:22:04 +1000

    game complete

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -13,19 +13,36 @@ const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
 //starting condition
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
-let currentScore = 0;
+
+let currentScore;
 
 //register the current player
-let activePlayer = 0;
+let activePlayer;
 //store total scores for both players.
-const scores = [0, 0];
+let scores;
 
 //state value for whether we will play the game or not, changed when one is winner.
 
-let playing = true;
+let playing;
+
+const init = function () {
+  currentScore = 0;
+  activePlayer = 0;
+  scores = [0, 0];
+  playing = true;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+
+  diceEl.classList.add('hidden');
+};
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -36,6 +53,8 @@ const switchPlayer = function () {
   player0El.classList.toggle('player--active');
   player1El.classList.toggle('player--active');
 };
+
+init();
 
 //Rolling dice functionility
 
@@ -75,7 +94,7 @@ btnHold.addEventListener('click', function () {
       playing = false;
       document
         .querySelector(`.player--${activePlayer}`)
-        .classList.add('.player--winner');
+        .classList.add('player--winner');
       document
         .querySelector(`.player--${activePlayer}`)
         .classList.remove('player--active');
@@ -86,3 +105,5 @@ btnHold.addEventListener('click', function () {
     }
   }
 });
+
+btnNew.addEventListener('click', init);
