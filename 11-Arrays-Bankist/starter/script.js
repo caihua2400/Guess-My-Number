b1ee6c68900adde993c08ad31ac68084b63a00df
caihua2300 <caihua2300@gmail.com>
2022-06-04 19:45:35 +1000

    update UI

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -61,10 +61,6 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// LECTURES
-
 const currencies = new Map([
   ['USD', 'United States dollar'],
   ['EUR', 'Euro'],
@@ -74,3 +70,243 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = '';
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+    const html = `
+    <div class="movements__row">
+    <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+    
+    <div class="movements__value">${mov}€</div>
+    </div>
+    
+    `;
+
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+
+const createUsernames = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(str => str.slice(0, 1))
+      .join('');
+  });
+};
+
+const calcDisplaySummary = function (account) {
+  console.log(account.movements);
+  const incomes = account.movements
+    .filter(mov => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = `${incomes}€`;
+
+  const outs = account.movements
+    .filter(mov => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = `${Math.abs(outs)}€`;
+
+  const interests = account.movements
+    .filter(mov => mov > 0)
+    .map(mov => (mov * account.interestRate) / 100)
+    .filter(interest => interest > 1)
+    .reduce((acc, interest) => acc + interest, 0);
+  labelSumInterest.textContent = `${interests}€`;
+};
+
+createUsernames(accounts);
+
+const euroToUsd = 1.1;
+
+const calcAndDisplayBalance = function (acc) {
+  acc.balance = acc.movements.reduce((acc, cur) => acc + cur, 0);
+
+  labelBalance.textContent = `${acc.balance}EUR`;
+};
+
+const updateUI = function (currentAccount) {
+  displayMovements(currentAccount.movements);
+
+  //display balance
+  calcAndDisplayBalance(currentAccount);
+  //display summary
+  calcDisplaySummary(currentAccount);
+};
+//Transfer Money Logic
+// 1 check the cuurent user has enough money(bigger than the transfer) and
+
+//event handlers
+//remember if this is a form button it will reload the page. for this behivor, we have to stop this action at this moment.
+
+//we have to access the login user later on in other sections, so we store it in a global variable.
+//Then we add negative moment to the current user and add positive moment to the receiver account.
+// Then we update the UI.
+const money = 500;
+console.log(-money);
+const transferMoney = function () {
+  //console.log('transfer money start to execute');
+  //console.log(currentAccount.movements.reduce((acc, mov) => acc + mov, 0));
+  //let result = currentAccount.movements.reduce((acc, mov) => acc + mov, 0);
+  //if (amount > 0 && result) {
+  //console.log('transfer could be based.');
+  //currentAccount.movements.push(-amount);
+  //receiverAccount.movements.push(amount);
+  //displayMovements(currentAccount.movements);
+  //display balance
+  //calcAndDisplayBalance(currentAccount.movements);
+  //display summary
+  //calcDisplaySummary(currentAccount);
+  //}
+};
+
+let currentAccount;
+btnLogin.addEventListener('click', function (e) {
+  //prevent form from submitting.
+  e.preventDefault();
+  //console.log('login clicked');
+  console.log(accounts);
+  currentAccount = accounts.find(
+    acc => acc.username === inputLoginUsername.value
+  );
+  console.log(currentAccount);
+  // always remember the number since the input of the value is a string.
+  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+    //display UI and welcome message.
+    labelWelcome.textContent = `Welcome back, ${
+      currentAccount.owner.split(' ')[0]
+    }`;
+    containerApp.style.opacity = 100;
+    //clear the input fields and lose focus.
+    inputLoginUsername.value = inputLoginPin.value = '';
+    inputLoginPin.blur();
+    updateUI(currentAccount);
+    console.log('Login success');
+  }
+});
+
+btnTransfer.addEventListener('click', function (e) {
+  e.preventDefault();
+  const amount = Number(inputTransferAmount.value);
+  //console.log(inputTransferAmount.value);
+  const receiverAcc = accounts.find(
+    acc => acc.username === inputTransferTo.value
+  );
+  console.log(amount, receiverAcc);
+  //transferMoney(currentAccount, receiverAcc, amount);
+  //window.location.reload();
+  inputTransferAmount.value = inputTransferTo.value = '';
+
+  if (
+    amount > 0 &&
+    receiverAcc &&
+    currentAccount.balance >= amount &&
+    currentAccount.username !== receiverAcc?.username
+  ) {
+    console.log('Transfer valid');
+    currentAccount.movements.push(-amount);
+    receiverAcc.movements.push(amount);
+    updateUI(currentAccount);
+  }
+});
+
+btnClose.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    inputClosePin.value = inputCloseUsername.value = '';
+    const index = accounts.findIndex(
+      acc => acc.username === currentAccount.username
+    );
+    console.log(index);
+    accounts.splice(index, 1);
+    //hide UI
+    containerApp.style.opacity = 0;
+  }
+});
+
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+  const amount = Number(inputLoanAmount.value);
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount / 10)) {
+    currentAccount.movements.push(amount);
+    updateUI(currentAccount);
+  }
+  inputLoanAmount.value = '';
+});
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+// LECTURES
+// const movementsDesc = movements.map((mov, i, arr) =>
+//   mov > 0
+//     ? `Movements ${i + 1} : You deposited ${mov}`
+//     : `Movements ${i + 1} : You withdrew ${Math.abs(mov)}`
+// );
+
+//const movementsUSD = movements.map(mov => mov * euroToUsd);
+//const deposits = movements.filter(mov => mov > 0);
+
+//const withdrawals = movements.filter(mov => mov < 0);
+//console.log(account1.movements);
+// const totalDepositsUSD = account1.movements
+//   .filter(mov => mov > 0)
+//   .map(mov => mov * 1.1)
+//   .reduce((acc, mov) => acc + mov, 0);
+// console.log(totalDepositsUSD);
+
+//Maximum value of the movements array
+
+/*
+const greatest = movements.reduce(
+  (acc, cur) => (cur > acc ? cur : acc),
+  movements[0]
+);
+*/
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+/* 
+Let's go back to Julia and Kate's study about dogs. This time, they want to convert dog ages to human ages and calculate the average age of the dogs in their study.
+
+Create a function 'calcAverageHumanAge', which accepts an arrays of dog's ages ('ages'), and does the following things in order:
+
+1. Calculate the dog age in human years using the following formula: if the dog is <= 2 years old, humanAge = 2 * dogAge. If the dog is > 2 years old, humanAge = 16 + dogAge * 4.
+2. Exclude all dogs that are less than 18 human years old (which is the same as keeping dogs that are at least 18 years old)
+3. Calculate the average human age of all adult dogs (you should already know from other challenges how we calculate averages 😉)
+4. Run the function for both test datasets
+
+TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
+TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+*/
+
+/*
+const ages = [5, 2, 4, 1, 15, 8, 3];
+const ages2 = [16, 6, 10, 5, 6, 1, 4];
+console.log(ages.length);
+const calcAverageHumanAge = function (ages) {
+  return ages
+    .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
+    .filter(age => age >= 18)
+    .reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
+  //console.log(humanAges);
+};
+
+console.log(calcAverageHumanAge(ages2));
+*/
+
+//find method.
+
+// const firstWithdrawal = movements.find(mov => mov < 0);
+// console.log(firstWithdrawal);
+// const account = accounts.find(acc => acc.owner === 'Jessica Davis');
+// console.log(account);
